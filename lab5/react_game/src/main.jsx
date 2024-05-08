91e4b11034fe5e7dfd21a9a8a5dbf288e15557f4
b1129025 <120005392+b1129025@users.noreply.github.com>
2024-05-08 17:00:09 +0800

    Add files via upload

---
+++ lab5/react_game/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import React_game from './reactgame'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <React_game />
+  </React.StrictMode>,
+)
